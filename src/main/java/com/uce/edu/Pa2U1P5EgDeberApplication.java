6c1464cddd0672d5792048f9400de8721395ee09
EPGualoto <epgualoto@uce.edu.ec>
2023-11-12 19:34:27 -0500

    mi primer deber commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5EgDeberApplication.java
@@ -0,0 +1,22 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5EgDeberApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5EgDeberApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola soy Erika Gualoto");
+		System.out.println("Deber 1 de github");
+		
+	}
+
+}
